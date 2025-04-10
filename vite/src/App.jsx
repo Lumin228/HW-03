27dd322139c232dd29f4776d4b5140a5b71c8392
Lumin228 <6786679@gmail.com>
2025-04-10 18:18:19 +0300

    formss

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,25 +1,61 @@
 import { useId, useState } from "react";
+import { Formik, Field, Form } from 'formik';
 
 import './App.css'
 
 function App() {
+  const [hasAccepted, setHasAccepted] = useState(false);
 
-  const [inputValue, setInputValue] = useState({x: "", y: ""});
+  const nameFirst = useId();
 
   const handleChange = (evt) => {
-    setInputValue({...inputValue, x: evt.target.value});
-  };
-  const handleChanges = (evt) => {
-    setInputValue({...inputValue, y: evt.target.value});
+    setHasAccepted(evt.target.checked);
+    console.log(evt.target.checked);
+    
   };
 
   return (
     <div>
-      <input type="text" value={inputValue.x} onChange={handleChange} />
-      <p>{inputValue.x}</p>
-      <input type="text" value={inputValue.y} onChange={handleChanges} />
-      <p>{inputValue.y}</p>
-    </div>
+    <h1>Sign Up</h1>
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        level: 'neutral',
+      }}
+      onSubmit={async (values, actions) => {
+        await new Promise((r) => setTimeout(r, 500));
+        console.log(values);
+        actions.resetForm()
+        
+      }}
+    >
+      <Form>
+      <label htmlFor="asd">Service satisfaction level</label>
+        <Field as="select" name="level" id="asd">
+          <option value="good">Good</option>
+          <option value="neutral">Neutral</option>
+          <option value="bad">Bad</option>
+        </Field>
+
+        <label htmlFor={nameFirst}>First Name</label>
+        <Field id={nameFirst} name="firstName" placeholder="Jane" />
+
+        <label htmlFor="qweq">Message</label>
+        <Field as="textarea" name="message" id="qweq" rows="5" />
+
+        <label htmlFor="email">Email</label>
+        <Field
+          id="email"
+          name="email"
+          placeholder="jane@acme.com"
+          type="email"
+        />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  </div>
   );
 }
 
